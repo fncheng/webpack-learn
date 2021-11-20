53ac7a4a9c966efcb82f80aba89b40cf9a546bc5
fncheng <dong862647669@gmail.com>
2021-11-20 13:56:25 +0800

    fix: webpack打包vue

---
+++ build/webpack.config.js
@@ -0,0 +1,10 @@
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  module: {
+    rules: [{ test: /\.vue$/, loader: 'vue-loader' }],
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
