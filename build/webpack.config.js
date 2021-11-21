4e5f143993746033b8496367f20673f97dde6d01
fncheng <dong862647669@gmail.com>
2021-11-21 13:45:03 +0800

    处理sass

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,9 +2,32 @@ const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   module: {
-    rules: [{ test: /\.vue$/, loader: 'vue-loader' }],
+    rules: [
+      { test: /\.vue$/, loader: 'vue-loader' },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-preset-env',{
+                    // 其他选项
+                  },],
+                ],
+              },
+            },
+          },
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
   },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
+  plugins: [new VueLoaderPlugin()],
 }
