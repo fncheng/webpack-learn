183b9d2a354356138e825b22bbef8fe8a9aa3881
fncheng <dong862647669@gmail.com>
2021-12-17 18:20:18 +0800

    .vue文件支持ts

---
+++ src/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'HelloWorld',
+  data() {
+    return {
+      list: [1, 2, 3],
+      msg: 'msg'
+    }
+  },
+  created() {
+    this.list.push(111)
+  },
+  methods: {
+    init() {
+    }
+  }
+})
+</script>
+
+<style></style>
